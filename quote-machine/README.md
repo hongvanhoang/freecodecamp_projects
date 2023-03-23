66cf6fb94274954c4733a5ecf7634a51b3f1d4e5
hongvanhoang <115954751+hongvanhoang@users.noreply.github.com>
2023-03-23 21:53:40 +0100

    Add files via upload

---
+++ quote-machine/README.md
@@ -0,0 +1,4 @@
+# Quote Machine
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hongvanhoang/pen/abaGBqx](https://codepen.io/hongvanhoang/pen/abaGBqx).
+
---
+++ quote-machine/src/script.js
@@ -0,0 +1,19 @@
+const generateQuote= function() {
+	const text= [{quote:"Imperfection is beauty, madness is genius and it’s better to be absolutely ridiculous than absolutely boring.", author:"Marilyn Monroe"},
+	{quote:"Study, learn more, learn forever.", author:"V. I. Lenin"}, 
+	{quote:"Nothing is more precious than independence and liberty.", author:"Ho Chi Minh"}, 
+	{quote:"Workers of the world, unite! You have nothing to lose but your chains!", author: "K. Marx & F. Engels"}, 
+	{quote:"The third world is not poor. You don't go to poor countries to make money. Most countries are rich. Only the people are poor. Ordinary people pay the costs of empire. These countries are not underdeveloped, they are over exploited.", author:"Michael Parenti"},	
+	{quote:"Never interrupt your enemy when he is making a mistake.", author:"Napoleon Bonaparte"}]
+	
+	let index = Math.floor(Math.random() * text.length);
+    document.getElementById("text").innerHTML = '"'+text[index].quote+'"';
+    document.getElementById("author").innerHTML = "-"+text[index].author;
+
+  document.getElementById("tweet-quote").href = "https://twitter.com/intent/tweet?text="+text[index].quote+"-author:"+text[index].author
+	}
+
+window.onload = function() {
+    generateQuote();
+    document.getElementById("new-quote").addEventListener('click', generateQuote);
+ }
